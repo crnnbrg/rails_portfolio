cda70332b660e945cf344dfc6d4a9558c00698d5
Your Name <youremail@domain.com>
2017-08-04 14:43:44 +0300

    added validations between comments and the project they belong to

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ActiveRecord::Base
+  has_many :comments
   validates :title, presence: true
 end
+s
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe Project do
   it { should validate_presence_of :title }
+  it { should have_many :comments }
 end
