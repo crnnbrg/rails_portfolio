c2dac8aa3ca96d032a028880c83825260ee3dade
Your Name <youremail@domain.com>
2017-08-04 12:48:54 +0300

    added a create controller action, received it in the views of the apps, added an error message if the project was not added and also added a link to on index to take me to new_project path

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,4 +13,19 @@ class ProjectsController < ApplicationController
     @project = Project.new
     render :new
   end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to projects_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title)
+  end
 end
