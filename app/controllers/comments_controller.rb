80f2fa8dbd3902197a1069ef8b032906861a2a59
Your Name <youremail@domain.com>
2017-08-04 15:09:30 +0300

    added a controller action method for creating a new comment to a project

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,22 @@
-class TasksController < ApplicationController
+class CommentsController < ApplicationController
   def new
     @project = Project.find(params[:project_id])
     @comment = @project.comments.new
+  end
+
+  def create
+    @project = Project.find(params[:project_id])
+    @comment = @project.comments.new(comment_params)
+    if @comment.save
+      redirect_to project_path(@comment.project)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:review)
   end
 end
