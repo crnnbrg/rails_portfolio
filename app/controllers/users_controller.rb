1c9d0ff6a3efdb49788aca4fc1fffa34979ca038
Your Name <youremail@domain.com>
2017-08-12 14:33:52 +0300

    changed the flash message for when a user is signedup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      flash[:notice] = 'User added successfully!'
+      flash[:notice] = 'Thanks for signing up!'
       redirect_to root_url
     else
       render :new
