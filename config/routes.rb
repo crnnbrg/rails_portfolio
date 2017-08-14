4c1acf2963ae830ce16489c10a5dd3fcb3c9d5ae
Your Name <youremail@domain.com>
2017-08-14 15:31:30 +0300

    genearated views for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :views
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
