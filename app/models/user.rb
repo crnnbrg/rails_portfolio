72b1babfa52c5a403bc1b5e9cfd32f463147b906
Your Name <youremail@domain.com>
2017-08-17 15:27:48 +0300

    generated devise controllers acne. and then just generated verifications controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :comments
+
+  validates_uniqueness_of :mobile_number
+  validates :mobile_number, phone: { possible: false, allow_blank: true, types: [:mobile] }
+
+  def needs_mobile_number_verifying?
+    if is_verified || mobile_number.nil?
+      false
+    else
+      true
+    end
+  end
 end
