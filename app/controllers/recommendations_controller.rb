a8fd7d4319174dd78cee4eaf42b07d332b691f77
Your Name <youremail@domain.com>
2017-08-24 08:20:08 +0300

    added an error msg in the view for adding a new  reccommendation

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,13 +1,31 @@
 class RecommendationsController < ApplicationController
   def index
     @recomendations = Recommendation.all
+    render :index
   end
 
   def show
     @recommendation = Recommendation.find(params[:id])
+    render :show
   end
 
   def new
     @recommendation = Recommendation.new
+    render :new
+  end
+
+  def create
+    @recommendation = Recommendation.new(recommendation_params)
+    if @recommendation.save
+      redirect_to recommendations_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recommendation_params
+    params.require(:recommendation).permit(:name, :job_description, :email, :phone)
   end
 end
