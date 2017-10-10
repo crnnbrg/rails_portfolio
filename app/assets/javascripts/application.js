34d4258567a8de515b6183cca64d9121f1912199
Your Name <youremail@domain.com>
2017-10-10 16:17:28 +0300

    edited the tawk to api

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,10 +16,8 @@
 //= require_tree .
 //= require bootstrap-sprockets
 //= require_tree .
-<!--Start of Tawk.to Script-->
-< script type = "text/javascript" >
-  var Tawk_API = Tawk_API || {},
-    Tawk_LoadStart = new Date();
+var Tawk_API = Tawk_API || {},
+  Tawk_LoadStart = new Date();
 (function() {
   var s1 = document.createElement("script"),
     s0 = document.getElementsByTagName("script")[0];
@@ -28,5 +26,4 @@
   s1.charset = 'UTF-8';
   s1.setAttribute('crossorigin', '*');
   s0.parentNode.insertBefore(s1, s0);
-})(); < /script>
-<!--End of Tawk.to Script-->
+})();
