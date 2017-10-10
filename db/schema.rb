fa908e86f33015d54ff17fa9819c12837cae5d68
Your Name <youremail@domain.com>
2017-10-10 13:03:43 +0300

    created a migration for description in the projects table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170823140227) do
+ActiveRecord::Schema.define(version: 20171010100250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20170823140227) do
     t.datetime "updated_at", null: false
     t.string "picture"
     t.string "skill"
+    t.string "description"
   end
 
   create_table "recommendations", force: :cascade do |t|
